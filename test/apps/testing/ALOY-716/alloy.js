ab08e19ef4db21253e922da4946bc6d43f592291
Sergey Volkov <s.volkov@netris.ru>
2020-02-18 12:49:13 +0300

    fix: ALOY-716

--- test/apps/testing/ALOY-716/alloy.js
+++ test/apps/testing/ALOY-716/alloy.js
@@ -4,7 +4,7 @@ for (var i = 0; i < 20; i++) {
 	models.push({
 		title: 'title #' + (i + 1),
 		subtitle: 'this is a boring subtitle',
-		image: '/' + Math.floor(Math.random() * 4) + '.png'
+		image: '/' + i + '.png'
 	});
 }
 collection.reset(models);
