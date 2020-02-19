6412828140044033b2e11da82f6206313c838ccc
Sergey Volkov <s.volkov@netris.ru>
2020-02-19 14:38:34 +0300

    feat(typescript): update test ALOY-263

--- test/apps/testing/ALOY-263/_generated/android/alloy/controllers/index.js
+++ test/apps/testing/ALOY-263/_generated/android/alloy/controllers/index.js
@@ -1,75 +1,26 @@
-var Alloy = require('/alloy'),
-Backbone = Alloy.Backbone,
-_ = Alloy._;
-
-
-
-
-function __processArg(obj, key) {
-  var arg = null;
-  if (obj) {
-    arg = obj[key] || null;
-  }
-  return arg;
-}
-
-function Controller() {
-
-  require('/controllers/' + 'BaseController').apply(this, Array.prototype.slice.call(arguments));
-  this.__controllerPath = 'index';
-  this.args = arguments[0] || {};
-
-  if (arguments[0]) {
-    var __parentSymbol = __processArg(arguments[0], '__parentSymbol');
-    var $model = __processArg(arguments[0], '$model');
-    var __itemTemplate = __processArg(arguments[0], '__itemTemplate');
-  }
-  var $ = this;
-  var exports = {};
-  var __defers = {};
-
-  // Generated code that must be executed before all UI and/or
-  // controller code. One example is all model and collection
-  // declarations from markup.
-
-
-  // <dataFunctions>
-
-  // </dataFunctionsCode>
-
-  // Generated UI code
-  $.__views["index"] = Ti.UI.createWindow(
-  { backgroundColor: "#fff", fullscreen: false, exitOnClose: true, id: "index" });
-
-  $.__views["index"] && $.addTopLevelView($.__views["index"]);
-  $.__views["txt"] = Ti.UI.createTextField(
-  { value: "July 4, 2014", id: "txt", top: 20 });
-
-  $.__views["index"].add($.__views["txt"]);
-  $.__views["picker"] = Ti.UI.createPicker(
-  { value: new Date("Wed Dec 17 2014 03:24:00 GMT+0300 (GMT+03:00)"), minDate: new Date("Fri Feb 08 2013 13:30:26 GMT+0400 (GMT+03:00)"), maxDate: new Date("Tue Nov 17 2015 00:00:00 GMT+0300 (GMT+03:00)"), format24: false, calendarViewShown: false, id: "picker", top: 50, type: Ti.UI.PICKER_TYPE_DATE });
-
-  $.__views["index"].add($.__views["picker"]);
-  exports.destroy = function () {};
-
-
-  // make all IDed elements in $.__views available right on the $ in a
-  // controller's internal code. Externally the IDed elements will
-  // be accessed with getView().
-  _.extend($, $.__views);
-
-  // Controller code directly from the developer's controller file
-  $.index.open();
-
-  // Generated code that must be executed after all UI and
-  // controller code. One example deferred event handlers whose
-  // functions are not defined until after the controller code
-  // is executed.
-
-
-  // Extend the $ instance with all functions and properties
-  // defined on the exports object.
-  _.extend($, exports);
-}
-
-module.exports = Controller;
+"use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+Object.defineProperty(exports, "__esModule", { value: true });
+var indexAbstract_1 = require("/controllers/indexAbstract");
+var index = /** @class */ (function (_super) {
+    __extends(index, _super);
+    function index() {
+        var _this = _super.call(this) || this;
+        _this.index.open();
+        return _this;
+    }
+    return index;
+}(indexAbstract_1.indexAbstract));
+exports.default = index;
