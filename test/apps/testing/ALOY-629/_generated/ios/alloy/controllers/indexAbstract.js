11bf6a194dd634bd6310a603d2b445c5bf023184
Sergey Volkov <s.volkov@netris.ru>
2020-02-19 17:13:44 +0300

    feat(typescript): update test ALOY-629

---
+++ test/apps/testing/ALOY-629/_generated/ios/alloy/controllers/indexAbstract.js
@@ -0,0 +1,62 @@
+"use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var BaseController_1 = __importDefault(require("/controllers/BaseController"));
+var indexAbstract = /** @class */ (function (_super) {
+    __extends(indexAbstract, _super);
+    function indexAbstract(args) {
+        var _this = _super.call(this) || this;
+        _this.args = args;
+        _this.__controllerPath = indexAbstract.controllerPath;
+        _this.__parentSymbol = args && args['__parentSymbol'] ? args['__parentSymbol'] : null;
+        _this.$model = args && args['$model'] ? args['$model'] : null;
+        _this.__itemTemplate = args && args['__itemTemplate'] ? args['__itemTemplate'] : null;
+        // Generated code that must be executed before all UI and/or
+        // controller code. One example is all model and collection
+        // declarations from markup.
+        // <precode>
+        // </precode>
+        // Generated UI code
+        // <viewCode>
+        _this["index"] = Ti.UI.createWindow({ backgroundColor: "#fff", id: "index", });
+        _this["index"] && _this.addTopLevelView(_this["index"]);
+        _this["notes"] = Ti.UI.createLabel({ top: 10, left: 15, right: 15, height: Ti.UI.SIZE, textAlign: "left", color: "#000", font: { fontSize: 14, fontWeight: "normal", }, text: 'Below you should see a red box with a rounded black border and white bold text inside. On iOS, it should say "global merging is working!" with a gray drop shadow. On all other platforms, it should say "you should only see this on non-iOS builds" with no drop shadow.', id: "notes", });
+        _this["index"].add(_this["notes"]);
+        _this["__alloyId0"] = Ti.UI.createLabel({ color: "#fff", font: { fontSize: 24, fontWeight: "bold", }, bottom: 15, height: 120, width: 300, textAlign: "center", text: "global merging is working!", backgroundColor: "#a00", borderWidth: 2, borderRadius: 8, borderColor: "#000", shadowColor: "#888", shadowOffset: { x: 2, y: 2, }, });
+        _this["index"].add(_this["__alloyId0"]);
+        // </viewCode>
+        // Populating $.__views for backward compatibility
+        // <__views>
+        _this.__views["notes"] = _this.notes;
+        _this.__views["__alloyId0"] = _this.__alloyId0;
+        _this.__views["index"] = _this.index;
+        return _this;
+        // </__view>
+        // Generated code that must be executed after all UI and
+        // controller code. One example deferred event handlers whose
+        // functions are not defined until after the controller code
+        // is executed.
+        // <postCode>
+        // </postcode>
+    }
+    indexAbstract.prototype.destroy = function () {
+    };
+    indexAbstract.controllerPath = 'index';
+    return indexAbstract;
+}(BaseController_1.default));
+exports.indexAbstract = indexAbstract;
