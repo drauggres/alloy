fb96b8e492b2d0f01178243ea6ff5b6c51b7350a
Sergey Volkov <s.volkov@netris.ru>
2020-02-19 18:20:55 +0300

    feat(typescript): update test ALOY-657

--- test/apps/testing/ALOY-657/_generated/ios/alloy/controllers/index.js
+++ test/apps/testing/ALOY-657/_generated/ios/alloy/controllers/index.js
@@ -1,82 +1,26 @@
-var Alloy = require('/alloy'),
-Backbone = Alloy.Backbone,
-_ = Alloy._;
-
-
-
-
-function __processArg(obj, key) {
-  var arg = null;
-  if (obj) {
-    arg = obj[key] || null;
-  }
-  return arg;
-}
-
-function Controller() {
-
-  require('/controllers/' + 'BaseController').apply(this, Array.prototype.slice.call(arguments));
-  this.__controllerPath = 'index';
-  this.args = arguments[0] || {};
-
-  if (arguments[0]) {
-    var __parentSymbol = __processArg(arguments[0], '__parentSymbol');
-    var $model = __processArg(arguments[0], '$model');
-    var __itemTemplate = __processArg(arguments[0], '__itemTemplate');
-  }
-  var $ = this;
-  var exports = {};
-  var __defers = {};
-
-  // Generated code that must be executed before all UI and/or
-  // controller code. One example is all model and collection
-  // declarations from markup.
-
-
-  // <dataFunctions>
-
-  // </dataFunctionsCode>
-
-  // Generated UI code
-  $.__views["index"] = Ti.UI.createWindow(
-  { backgroundColor: "#fff", fullscreen: false, exitOnClose: true, id: "index" });
-
-  $.__views["index"] && $.addTopLevelView($.__views["index"]);
-  if (true) {
-    var __alloyId2 = [];$.__views["__alloyId3"] = Ti.UI.createButton({ systemButton: Ti.UI.iOS.SystemButton.FIXED_SPACE });__alloyId2.push($.__views["__alloyId3"]);$.__views["__alloyId4"] = Ti.UI.createButton(
-    { systemButton: Titanium.UI.iPhone.SystemButton.EDIT });
-
-    __alloyId2.push($.__views["__alloyId4"]);$.__views["__alloyId5"] = Ti.UI.createButton({ systemButton: Ti.UI.iOS.SystemButton.FIXED_SPACE });__alloyId2.push($.__views["__alloyId5"]);$.__views["__alloyId6"] = Ti.UI.createButton(
-    { systemButton: Titanium.UI.iPhone.SystemButton.ADD });
-
-    __alloyId2.push($.__views["__alloyId6"]);$.__views["__alloyId7"] = Ti.UI.createButton({ systemButton: Ti.UI.iOS.SystemButton.FIXED_SPACE, width: '100' });__alloyId2.push($.__views["__alloyId7"]);$.__views["__alloyId8"] = Ti.UI.createButton(
-    { systemButton: Titanium.UI.iPhone.SystemButton.DONE });
-
-    __alloyId2.push($.__views["__alloyId8"]);$.__views["__alloyId0"] = Ti.UI.createToolbar(
-    { items: __alloyId2 });
-
-    $.__views["index"].add($.__views["__alloyId0"]);
-  }
-  exports.destroy = function () {};
-
-
-  // make all IDed elements in $.__views available right on the $ in a
-  // controller's internal code. Externally the IDed elements will
-  // be accessed with getView().
-  _.extend($, $.__views);
-
-  // Controller code directly from the developer's controller file
-  $.index.open();
-
-  // Generated code that must be executed after all UI and
-  // controller code. One example deferred event handlers whose
-  // functions are not defined until after the controller code
-  // is executed.
-
-
-  // Extend the $ instance with all functions and properties
-  // defined on the exports object.
-  _.extend($, exports);
-}
-
-module.exports = Controller;
+"use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+Object.defineProperty(exports, "__esModule", { value: true });
+var indexAbstract_1 = require("/controllers/indexAbstract");
+var index = /** @class */ (function (_super) {
+    __extends(index, _super);
+    function index() {
+        var _this = _super.call(this) || this;
+        _this.index.open();
+        return _this;
+    }
+    return index;
+}(indexAbstract_1.indexAbstract));
+exports.default = index;
