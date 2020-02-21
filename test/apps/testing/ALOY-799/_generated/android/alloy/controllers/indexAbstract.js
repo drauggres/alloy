348661bb83493c001dd2d001ee43fca7593a0ada
Sergey Volkov <s.volkov@netris.ru>
2020-02-21 14:13:56 +0300

    feat(typescript): update test ALOY-799

---
+++ test/apps/testing/ALOY-799/_generated/android/alloy/controllers/indexAbstract.js
@@ -0,0 +1,66 @@
+"use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var alloy_1 = __importDefault(require("alloy"));
+var BaseController_1 = __importDefault(require("/controllers/BaseController"));
+var indexAbstract = /** @class */ (function (_super) {
+    __extends(indexAbstract, _super);
+    function indexAbstract(args) {
+        var _this = _super.call(this) || this;
+        _this.args = args;
+        _this.__controllerPath = indexAbstract.controllerPath;
+        _this.__parentSymbol = args && args['__parentSymbol'] ? args['__parentSymbol'] : null;
+        _this.$model = args && args['$model'] ? args['$model'] : null;
+        _this.__itemTemplate = args && args['__itemTemplate'] ? args['__itemTemplate'] : null;
+        // Generated code that must be executed before all UI and/or
+        // controller code. One example is all model and collection
+        // declarations from markup.
+        // <precode>
+        // </precode>
+        // Generated UI code
+        // <viewCode>
+        _this["index"] = Ti.UI.createWindow({ backgroundColor: "#fff", fullscreen: false, exitOnClose: true, layout: "vertical", id: "index", });
+        _this["index"] && _this.addTopLevelView(_this["index"]);
+        _this["__alloyId0"] = alloy_1.default.createWidget('widget1', 'widget', { __parentSymbol: _this["index"], });
+        _this["__alloyId0"].setParent(_this["index"]);
+        _this["__alloyId1"] = alloy_1.default.createWidget('widget2', 'widget', { __parentSymbol: _this["index"], });
+        _this["__alloyId1"].setParent(_this["index"]);
+        _this["__alloyId2"] = alloy_1.default.createController('widget', { __parentSymbol: _this["index"], });
+        _this["__alloyId2"].setParent(_this["index"]);
+        // </viewCode>
+        // Populating $.__views for backward compatibility
+        // <__views>
+        _this.__views["__alloyId0"] = _this.__alloyId0;
+        _this.__views["__alloyId1"] = _this.__alloyId1;
+        _this.__views["__alloyId2"] = _this.__alloyId2;
+        _this.__views["index"] = _this.index;
+        return _this;
+        // </__view>
+        // Generated code that must be executed after all UI and
+        // controller code. One example deferred event handlers whose
+        // functions are not defined until after the controller code
+        // is executed.
+        // <postCode>
+        // </postcode>
+    }
+    indexAbstract.prototype.destroy = function () {
+    };
+    indexAbstract.controllerPath = 'index';
+    return indexAbstract;
+}(BaseController_1.default));
+exports.indexAbstract = indexAbstract;
