26d567501ac6a34dfe368d3a0eb533d8bac6a87d
Sergey Volkov <s.volkov@netris.ru>
2020-02-25 19:22:00 +0300

    feat(typescript): update test ALOY-1055

---
+++ test/apps/testing/ALOY-1055/_generated/android/alloy/controllers/indexAbstract.js
@@ -0,0 +1,123 @@
+"use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var BaseController_1 = __importDefault(require("/controllers/BaseController"));
+var indexAbstract = /** @class */ (function (_super) {
+    __extends(indexAbstract, _super);
+    function indexAbstract(args) {
+        var _this = _super.call(this) || this;
+        _this.args = args;
+        _this.__controllerPath = indexAbstract.controllerPath;
+        _this.__parentSymbol = args && args['__parentSymbol'] ? args['__parentSymbol'] : null;
+        _this.$model = args && args['$model'] ? args['$model'] : null;
+        _this.__itemTemplate = args && args['__itemTemplate'] ? args['__itemTemplate'] : null;
+        // Generated code that must be executed before all UI and/or
+        // controller code. One example is all model and collection
+        // declarations from markup.
+        // <precode>
+        // </precode>
+        // Generated UI code
+        // <viewCode>
+        _this["index"] = Ti.UI.createWindow({ backgroundColor: "#fff", fullscreen: false, exitOnClose: true, id: "index", });
+        _this["index"] && _this.addTopLevelView(_this["index"]);
+        var __alloyId1 = [];
+        var __alloyId2 = {};
+        var __alloyId5 = [];
+        var __alloyId6 = { type: 'Ti.UI.Label', bindId: 'info', properties: { color: "black", font: { fontFamily: "Arial", fontSize: "20dp", fontWeight: "bold", }, left: "10dp", top: 0, bindId: "info", }, };
+        __alloyId5.push(__alloyId6);
+        var __alloyId7 = { type: 'Ti.UI.Label', bindId: 'es_info', properties: { color: "gray", font: { fontFamily: "Arial", fontSize: "14dp", }, left: "10dp", top: "25dp", bindId: "es_info", }, };
+        __alloyId5.push(__alloyId7);
+        var __alloyId4 = { properties: { name: "template", }, childTemplates: __alloyId5, };
+        __alloyId2["template"] = __alloyId4;
+        var __alloyId10 = [];
+        _this["__alloyId11"] = { info: { text: "Apple", }, es_info: { text: "Manzana", }, };
+        __alloyId10.push(_this["__alloyId11"]);
+        _this["__alloyId12"] = { info: { text: "Banana", }, es_info: { text: "Banana", }, };
+        __alloyId10.push(_this["__alloyId12"]);
+        _this["__alloyId8"] = Ti.UI.createListSection({ headerTitle: "Fruit / Frutas", });
+        _this["__alloyId8"].items = __alloyId10;
+        var __alloyId13 = [];
+        __alloyId13.push(_this["__alloyId8"]);
+        var __alloyId16 = [];
+        _this["__alloyId17"] = { info: { text: "Carrot", }, es_info: { text: "Zanahoria", }, };
+        __alloyId16.push(_this["__alloyId17"]);
+        _this["__alloyId18"] = { info: { text: "Potato", }, es_info: { text: "Patata", }, };
+        __alloyId16.push(_this["__alloyId18"]);
+        _this["__alloyId14"] = Ti.UI.createListSection({ headerTitle: "Vegetables / Verduras", });
+        _this["__alloyId14"].items = __alloyId16;
+        __alloyId13.push(_this["__alloyId14"]);
+        var __alloyId21 = [];
+        _this["__alloyId22"] = { info: { text: "Corn", }, es_info: { text: "Maiz", }, };
+        __alloyId21.push(_this["__alloyId22"]);
+        _this["__alloyId23"] = { info: { text: "Rice", }, es_info: { text: "Arroz", }, };
+        __alloyId21.push(_this["__alloyId23"]);
+        _this["__alloyId19"] = Ti.UI.createListSection({ headerTitle: "Grains / Granos", });
+        _this["__alloyId19"].items = __alloyId21;
+        __alloyId13.push(_this["__alloyId19"]);
+        _this["listView"] = Ti.UI.createListView({ sections: __alloyId13, templates: __alloyId2, id: "listView", defaultItemTemplate: "template", });
+        __alloyId1.push(_this["listView"]);
+        var __alloyId26 = [];
+        _this["__alloyId27"] = { properties: { title: "List item 1", }, };
+        __alloyId26.push(_this["__alloyId27"]);
+        _this["__alloyId28"] = { properties: { title: "List item 2", }, };
+        __alloyId26.push(_this["__alloyId28"]);
+        _this["__alloyId29"] = { properties: { title: "List item 3", }, };
+        __alloyId26.push(_this["__alloyId29"]);
+        _this["__alloyId24"] = Ti.UI.createListSection({});
+        _this["__alloyId24"].items = __alloyId26;
+        var __alloyId30 = [];
+        __alloyId30.push(_this["__alloyId24"]);
+        _this["list"] = Ti.UI.createListView({ sections: __alloyId30, id: "list", });
+        __alloyId1.push(_this["list"]);
+        _this["__alloyId0"] = Ti.UI.createScrollableView({ views: __alloyId1, });
+        _this["index"].add(_this["__alloyId0"]);
+        // </viewCode>
+        // Populating $.__views for backward compatibility
+        // <__views>
+        _this.__views["__alloyId11"] = _this.__alloyId11;
+        _this.__views["__alloyId12"] = _this.__alloyId12;
+        _this.__views["__alloyId8"] = _this.__alloyId8;
+        _this.__views["__alloyId17"] = _this.__alloyId17;
+        _this.__views["__alloyId18"] = _this.__alloyId18;
+        _this.__views["__alloyId14"] = _this.__alloyId14;
+        _this.__views["__alloyId22"] = _this.__alloyId22;
+        _this.__views["__alloyId23"] = _this.__alloyId23;
+        _this.__views["__alloyId19"] = _this.__alloyId19;
+        _this.__views["listView"] = _this.listView;
+        _this.__views["__alloyId27"] = _this.__alloyId27;
+        _this.__views["__alloyId28"] = _this.__alloyId28;
+        _this.__views["__alloyId29"] = _this.__alloyId29;
+        _this.__views["__alloyId24"] = _this.__alloyId24;
+        _this.__views["list"] = _this.list;
+        _this.__views["__alloyId0"] = _this.__alloyId0;
+        _this.__views["index"] = _this.index;
+        return _this;
+        // </__view>
+        // Generated code that must be executed after all UI and
+        // controller code. One example deferred event handlers whose
+        // functions are not defined until after the controller code
+        // is executed.
+        // <postCode>
+        // </postcode>
+    }
+    indexAbstract.prototype.destroy = function () {
+    };
+    indexAbstract.controllerPath = 'index';
+    return indexAbstract;
+}(BaseController_1.default));
+exports.indexAbstract = indexAbstract;
--- test/apps/testing/ALOY-1055/_generated/ios/alloy/controllers/index.js
+++ test/apps/testing/ALOY-1055/_generated/ios/alloy/controllers/index.js
@@ -1,92 +1,26 @@
-var Alloy = require('/alloy'),
-Backbone = Alloy.Backbone,
-_ = Alloy._;
-
-
-
-
-function __processArg(obj, key) {
-  var arg = null;
-  if (obj) {
-    arg = obj[key] || null;
-  }
-  return arg;
-}
-
-function Controller() {
-
-  require('/controllers/' + 'BaseController').apply(this, Array.prototype.slice.call(arguments));
-  this.__controllerPath = 'index';
-  this.args = arguments[0] || {};
-
-  if (arguments[0]) {
-    var __parentSymbol = __processArg(arguments[0], '__parentSymbol');
-    var $model = __processArg(arguments[0], '$model');
-    var __itemTemplate = __processArg(arguments[0], '__itemTemplate');
-  }
-  var $ = this;
-  var exports = {};
-  var __defers = {};
-
-  // Generated code that must be executed before all UI and/or
-  // controller code. One example is all model and collection
-  // declarations from markup.
-
-
-  // <dataFunctions>
-
-  // </dataFunctionsCode>
-
-  // Generated UI code
-  $.__views["index"] = Ti.UI.createWindow(
-  { backgroundColor: "#fff", fullscreen: false, exitOnClose: true, id: "index" });
-
-  $.__views["index"] && $.addTopLevelView($.__views["index"]);
-  var __alloyId1 = [];
-  var __alloyId2 = {};var __alloyId5 = [];var __alloyId6 = { type: 'Ti.UI.Label', bindId: 'info', properties: { color: "black", font: { fontFamily: "Arial", fontSize: "20dp", fontWeight: "bold" }, left: "10dp", top: 0, bindId: "info" } };__alloyId5.push(__alloyId6);var __alloyId7 = { type: 'Ti.UI.Label', bindId: 'es_info', properties: { color: "gray", font: { fontFamily: "Arial", fontSize: "14dp" }, left: "10dp", top: "25dp", bindId: "es_info" } };__alloyId5.push(__alloyId7);var __alloyId4 = { properties: { name: "template" }, childTemplates: __alloyId5 };__alloyId2["template"] = __alloyId4;var __alloyId10 = [];$.__views["__alloyId11"] = { info: { text: "Apple" }, es_info: { text: "Manzana" } };__alloyId10.push($.__views["__alloyId11"]);$.__views["__alloyId12"] = { info: { text: "Banana" }, es_info: { text: "Banana" } };__alloyId10.push($.__views["__alloyId12"]);$.__views["__alloyId8"] = Ti.UI.createListSection(
-  { headerTitle: "Fruit / Frutas" });
-
-  $.__views["__alloyId8"].items = __alloyId10;var __alloyId13 = [];__alloyId13.push($.__views["__alloyId8"]);var __alloyId16 = [];$.__views["__alloyId17"] = { info: { text: "Carrot" }, es_info: { text: "Zanahoria" } };__alloyId16.push($.__views["__alloyId17"]);$.__views["__alloyId18"] = { info: { text: "Potato" }, es_info: { text: "Patata" } };__alloyId16.push($.__views["__alloyId18"]);$.__views["__alloyId14"] = Ti.UI.createListSection(
-  { headerTitle: "Vegetables / Verduras" });
-
-  $.__views["__alloyId14"].items = __alloyId16;__alloyId13.push($.__views["__alloyId14"]);var __alloyId21 = [];$.__views["__alloyId22"] = { info: { text: "Corn" }, es_info: { text: "Maiz" } };__alloyId21.push($.__views["__alloyId22"]);$.__views["__alloyId23"] = { info: { text: "Rice" }, es_info: { text: "Arroz" } };__alloyId21.push($.__views["__alloyId23"]);$.__views["__alloyId19"] = Ti.UI.createListSection(
-  { headerTitle: "Grains / Granos" });
-
-  $.__views["__alloyId19"].items = __alloyId21;__alloyId13.push($.__views["__alloyId19"]);$.__views["listView"] = Ti.UI.createListView(
-  { sections: __alloyId13, templates: __alloyId2, id: "listView", defaultItemTemplate: "template" });
-
-  __alloyId1.push($.__views["listView"]);
-  var __alloyId26 = [];$.__views["__alloyId27"] = { properties: { title: "List item 1" } };__alloyId26.push($.__views["__alloyId27"]);$.__views["__alloyId28"] = { properties: { title: "List item 2" } };__alloyId26.push($.__views["__alloyId28"]);$.__views["__alloyId29"] = { properties: { title: "List item 3" } };__alloyId26.push($.__views["__alloyId29"]);$.__views["__alloyId24"] = Ti.UI.createListSection(
-  {});
-
-  $.__views["__alloyId24"].items = __alloyId26;var __alloyId30 = [];__alloyId30.push($.__views["__alloyId24"]);$.__views["list"] = Ti.UI.createListView(
-  { sections: __alloyId30, id: "list" });
-
-  __alloyId1.push($.__views["list"]);
-  $.__views["__alloyId0"] = Ti.UI.createScrollableView(
-  { top: 20, showPagingControl: true, pagingControlColor: "black", views: __alloyId1 });
-
-  $.__views["index"].add($.__views["__alloyId0"]);
-  exports.destroy = function () {};
-
-
-  // make all IDed elements in $.__views available right on the $ in a
-  // controller's internal code. Externally the IDed elements will
-  // be accessed with getView().
-  _.extend($, $.__views);
-
-  // Controller code directly from the developer's controller file
-  $.index.open();
-
-  // Generated code that must be executed after all UI and
-  // controller code. One example deferred event handlers whose
-  // functions are not defined until after the controller code
-  // is executed.
-
-
-  // Extend the $ instance with all functions and properties
-  // defined on the exports object.
-  _.extend($, exports);
-}
-
-module.exports = Controller;
+"use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+Object.defineProperty(exports, "__esModule", { value: true });
+var indexAbstract_1 = require("/controllers/indexAbstract");
+var index = /** @class */ (function (_super) {
+    __extends(index, _super);
+    function index() {
+        var _this = _super.call(this) || this;
+        _this.index.open();
+        return _this;
+    }
+    return index;
+}(indexAbstract_1.indexAbstract));
+exports.default = index;
