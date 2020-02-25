f7281d615ae5fedf57d1f5e5a27ecc27f1a77966
Sergey Volkov <s.volkov@netris.ru>
2020-02-25 17:54:57 +0300

    feat(typescript): update test ALOY-991

--- test/apps/testing/ALOY-991/_generated/android/alloy/controllers/index.js
+++ test/apps/testing/ALOY-991/_generated/android/alloy/controllers/index.js
@@ -1,113 +1,32 @@
-var Alloy = require('/alloy'),
-Backbone = Alloy.Backbone,
-_ = Alloy._;
-
-
-
-
-function __processArg(obj, key) {
-  var arg = null;
-  if (obj) {
-    arg = obj[key] || null;
-  }
-  return arg;
-}
-
-function Controller() {
-
-  require('/controllers/' + 'BaseController').apply(this, Array.prototype.slice.call(arguments));
-  this.__controllerPath = 'index';
-  this.args = arguments[0] || {};
-
-  if (arguments[0]) {
-    var __parentSymbol = __processArg(arguments[0], '__parentSymbol');
-    var $model = __processArg(arguments[0], '$model');
-    var __itemTemplate = __processArg(arguments[0], '__itemTemplate');
-  }
-  var $ = this;
-  var exports = {};
-  var __defers = {};
-
-  // Generated code that must be executed before all UI and/or
-  // controller code. One example is all model and collection
-  // declarations from markup.
-
-
-  // <dataFunctions>
-
-  // </dataFunctionsCode>
-
-  // Generated UI code
-  $.__views["index"] = Ti.UI.createWindow(
-  { title: "ALOY-991", id: "index" });
-
-  $.__views["index"] && $.addTopLevelView($.__views["index"]);
-  if (true) {
-    const __alloyId2 = () => {
-      $.__views["index"].removeEventListener('open', __alloyId2);
-      if ($.__views["index"].activity) {
-        $.__views["index"].activity.onCreateOptionsMenu = e => {
-          var __alloyId0 = { showAsAction: Ti.Android.SHOW_AS_ACTION_ALWAYS, title: "One", id: "item1" };
-          if (false) {
-            __alloyId0.actionView = false;
-          }
-          if (e.menu) {
-            $.__views["item1"] = e.menu.add(_.pick(__alloyId0, Alloy.Android.menuItemCreateArgs));
-          }
-          if ($.__views["item1"]) {
-            $.__views["item1"].applyProperties(_.omit(__alloyId0, Alloy.Android.menuItemCreateArgs));
-          }
-          doMenuClick ? $.addListener($.__views["item1"], 'click', doMenuClick) : __defers['$.__views["item1"]!click!doMenuClick'] = true;var __alloyId1 = { showAsAction: Ti.Android.SHOW_AS_ACTION_ALWAYS, title: "Two", icon: Ti.Android.R.drawable.ic_menu_share, id: "item2" };
-          if (false) {
-            __alloyId1.actionView = false;
-          }
-          if (e.menu) {
-            $.__views["item2"] = e.menu.add(_.pick(__alloyId1, Alloy.Android.menuItemCreateArgs));
-          }
-          if ($.__views["item2"]) {
-            $.__views["item2"].applyProperties(_.omit(__alloyId1, Alloy.Android.menuItemCreateArgs));
-          }
-          doMenuClick ? $.addListener($.__views["item2"], 'click', doMenuClick) : __defers['$.__views["item2"]!click!doMenuClick'] = true;$.__views["index"].activity.actionBar.title = "Menu Title";$.__views["index"].activity.actionBar.subtitle = "menu subtitle";$.__views["index"].activity.actionBar.displayHomeAsUp = true;
-        };
-      } else {
-        Ti.API.warn('You attempted to attach an Android Menu to a lightweight Window');
-        Ti.API.warn('or other UI component which does not have an Android activity.');
-        Ti.API.warn('Android Menus can only be opened on TabGroups and heavyweight Windows.');
-      }
+"use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
     };
-    $.__views["index"].addEventListener('open', __alloyId2);
-  }
-  $.__views["label"] = Ti.UI.createLabel(
-  { width: Ti.UI.SIZE, height: Ti.UI.SIZE, color: "#fff", text: 'Hello! Click Me!', id: "label" });
-
-  $.__views["index"].add($.__views["label"]);
-  doClick ? $.addListener($.__views["label"], 'click', doClick) : __defers['$.__views["label"]!click!doClick'] = true;exports.destroy = function () {};
-
-
-  // make all IDed elements in $.__views available right on the $ in a
-  // controller's internal code. Externally the IDed elements will
-  // be accessed with getView().
-  _.extend($, $.__views);
-
-  // Controller code directly from the developer's controller file
-  function doClick() {
-    alert('label clicked');
-  }
-  function doMenuClick() {
-    alert('menu clicked');
-  }
-
-  $.index.open();
-
-  // Generated code that must be executed after all UI and
-  // controller code. One example deferred event handlers whose
-  // functions are not defined until after the controller code
-  // is executed.
-  __defers['$.__views["item1"]!click!doMenuClick'] && $.addListener($.__views["item1"], 'click', doMenuClick);__defers['$.__views["item2"]!click!doMenuClick'] && $.addListener($.__views["item2"], 'click', doMenuClick);__defers['$.__views["label"]!click!doClick'] && $.addListener($.__views["label"], 'click', doClick);
-
-  // Extend the $ instance with all functions and properties
-  // defined on the exports object.
-  _.extend($, exports);
-}
-
-module.exports = Controller;
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+Object.defineProperty(exports, "__esModule", { value: true });
+var indexAbstract_1 = require("/controllers/indexAbstract");
+var index = /** @class */ (function (_super) {
+    __extends(index, _super);
+    function index() {
+        var _this = _super.call(this) || this;
+        _this.index.open();
+        return _this;
+    }
+    index.prototype.doClick = function () {
+        alert('label clicked');
+    };
+    index.prototype.doMenuClick = function () {
+        alert('menu clicked');
+    };
+    return index;
+}(indexAbstract_1.indexAbstract));
+exports.default = index;
