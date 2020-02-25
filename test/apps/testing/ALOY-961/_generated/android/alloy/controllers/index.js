f2184e811971369d43e94b874aaab784c57c7eb1
Sergey Volkov <s.volkov@netris.ru>
2020-02-25 15:50:03 +0300

    feat(typescript): update test ALOY-961

--- test/apps/testing/ALOY-961/_generated/android/alloy/controllers/index.js
+++ test/apps/testing/ALOY-961/_generated/android/alloy/controllers/index.js
@@ -1,91 +1,26 @@
-var Alloy = require('/alloy'),
-Backbone = Alloy.Backbone,
-_ = Alloy._;
-
-
-
-
-function __processArg(obj, key) {
-  var arg = null;
-  if (obj) {
-    arg = obj[key] || null;
-  }
-  return arg;
-}
-
-function Controller() {
-
-  require('/controllers/' + 'BaseController').apply(this, Array.prototype.slice.call(arguments));
-  this.__controllerPath = 'index';
-  this.args = arguments[0] || {};
-
-  if (arguments[0]) {
-    var __parentSymbol = __processArg(arguments[0], '__parentSymbol');
-    var $model = __processArg(arguments[0], '$model');
-    var __itemTemplate = __processArg(arguments[0], '__itemTemplate');
-  }
-  var $ = this;
-  var exports = {};
-  var __defers = {};
-
-  // Generated code that must be executed before all UI and/or
-  // controller code. One example is all model and collection
-  // declarations from markup.
-
-
-  // <dataFunctions>
-
-  // </dataFunctionsCode>
-
-  // Generated UI code
-  $.__views["index"] = Ti.UI.createWindow(
-  { backgroundColor: "#fff", layout: "vertical", id: "index" });
-
-  $.__views["index"] && $.addTopLevelView($.__views["index"]);
-  $.__views["__alloyId1"] = Ti.UI.createAttributedString(
-  { attributes: [{ type: Ti.UI.ATTRIBUTE_UNDERLINES_STYLE, value: Ti.UI.ATTRIBUTE_UNDERLINE_STYLE_SINGLE, range: [0, 107] }, { type: Ti.UI.ATTRIBUTE_BACKGROUND_COLOR, value: "red", range: [18, 12] }, { type: Ti.UI.ATTRIBUTE_BACKGROUND_COLOR, value: "blue", range: [31, 2] }, { type: Ti.UI.ATTRIBUTE_BACKGROUND_COLOR, value: "yellow", range: [40, 6] }, { type: Ti.UI.ATTRIBUTE_FOREGROUND_COLOR, value: "orange", range: [0, 107] }, { type: Ti.UI.ATTRIBUTE_FOREGROUND_COLOR, value: "black", range: [40, 6] }], text: 'Label - Bacon ipsum dolor Appcelerator Titanium rocks! sit amet fatback leberkas salami sausage tongue strip steak.' });
-
-  $.__views["__alloyId0"] = Ti.UI.createLabel(
-  { top: 20, right: 10, left: 10, width: Ti.UI.FILL, height: Ti.UI.SIZE, attributedString: $.__views["__alloyId1"] });
-
-  $.__views["index"].add($.__views["__alloyId0"]);
-  $.__views["__alloyId3"] = Ti.UI.createAttributedString(
-  { attributes: [{ type: Ti.UI.ATTRIBUTE_UNDERLINES_STYLE, value: Ti.UI.ATTRIBUTE_UNDERLINE_STYLE_SINGLE, range: [0, 107] }, { type: Ti.UI.ATTRIBUTE_BACKGROUND_COLOR, value: "red", range: [18, 12] }, { type: Ti.UI.ATTRIBUTE_BACKGROUND_COLOR, value: "blue", range: [31, 2] }, { type: Ti.UI.ATTRIBUTE_BACKGROUND_COLOR, value: "yellow", range: [40, 6] }, { type: Ti.UI.ATTRIBUTE_FOREGROUND_COLOR, value: "orange", range: [0, 107] }, { type: Ti.UI.ATTRIBUTE_FOREGROUND_COLOR, value: "black", range: [40, 6] }], text: 'TextField - Bacon ipsum dolor Appcelerator Titanium rocks! sit amet fatback leberkas salami sausage tongue strip steak.' });
-
-  $.__views["__alloyId4"] = Ti.UI.createAttributedString(
-  { attributes: [{ type: Ti.UI.ATTRIBUTE_UNDERLINES_STYLE, value: Ti.UI.ATTRIBUTE_UNDERLINE_STYLE_SINGLE, range: [0, 107] }, { type: Ti.UI.ATTRIBUTE_BACKGROUND_COLOR, value: "red", range: [18, 12] }, { type: Ti.UI.ATTRIBUTE_BACKGROUND_COLOR, value: "blue", range: [31, 2] }, { type: Ti.UI.ATTRIBUTE_BACKGROUND_COLOR, value: "yellow", range: [40, 6] }, { type: Ti.UI.ATTRIBUTE_FOREGROUND_COLOR, value: "orange", range: [0, 107] }, { type: Ti.UI.ATTRIBUTE_FOREGROUND_COLOR, value: "black", range: [40, 6] }], text: 'TextField HintText - Bacon ipsum dolor Appcelerator Titanium rocks! sit amet fatback leberkas salami sausage tongue strip steak.' });
-
-  $.__views["__alloyId2"] = Ti.UI.createTextField(
-  { top: 20, right: 10, left: 10, width: Ti.UI.FILL, height: Ti.UI.SIZE, attributedString: $.__views["__alloyId3"], attributedHintText: $.__views["__alloyId4"] });
-
-  $.__views["index"].add($.__views["__alloyId2"]);
-  $.__views["__alloyId6"] = Ti.UI.createAttributedString(
-  { attributes: [{ type: Ti.UI.ATTRIBUTE_UNDERLINES_STYLE, value: Ti.UI.ATTRIBUTE_UNDERLINE_STYLE_SINGLE, range: [0, 107] }, { type: Ti.UI.ATTRIBUTE_BACKGROUND_COLOR, value: "red", range: [18, 12] }, { type: Ti.UI.ATTRIBUTE_BACKGROUND_COLOR, value: "blue", range: [31, 2] }, { type: Ti.UI.ATTRIBUTE_BACKGROUND_COLOR, value: "yellow", range: [40, 6] }, { type: Ti.UI.ATTRIBUTE_FOREGROUND_COLOR, value: "orange", range: [0, 107] }, { type: Ti.UI.ATTRIBUTE_FOREGROUND_COLOR, value: "black", range: [40, 6] }], text: 'TextArea - Bacon ipsum dolor Appcelerator Titanium rocks! sit amet fatback leberkas salami sausage tongue strip steak.' });
-
-  $.__views["__alloyId5"] = Ti.UI.createTextArea(
-  { top: 20, right: 10, left: 10, width: Ti.UI.FILL, height: 200, attributedString: $.__views["__alloyId6"] });
-
-  $.__views["index"].add($.__views["__alloyId5"]);
-  exports.destroy = function () {};
-
-
-  // make all IDed elements in $.__views available right on the $ in a
-  // controller's internal code. Externally the IDed elements will
-  // be accessed with getView().
-  _.extend($, $.__views);
-
-  // Controller code directly from the developer's controller file
-  $.index.open();
-
-  // Generated code that must be executed after all UI and
-  // controller code. One example deferred event handlers whose
-  // functions are not defined until after the controller code
-  // is executed.
-
-
-  // Extend the $ instance with all functions and properties
-  // defined on the exports object.
-  _.extend($, exports);
-}
-
-module.exports = Controller;
+"use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+Object.defineProperty(exports, "__esModule", { value: true });
+var indexAbstract_1 = require("/controllers/indexAbstract");
+var index = /** @class */ (function (_super) {
+    __extends(index, _super);
+    function index() {
+        var _this = _super.call(this) || this;
+        _this.index.open();
+        return _this;
+    }
+    return index;
+}(indexAbstract_1.indexAbstract));
+exports.default = index;
