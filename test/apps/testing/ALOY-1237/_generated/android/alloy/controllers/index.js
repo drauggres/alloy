f31597c0cd875d4abacbc10b40c8d9f7693063a5
Sergey Volkov <s.volkov@netris.ru>
2020-02-26 13:25:50 +0300

    feat(typescript): update test ALOY-1237

--- test/apps/testing/ALOY-1237/_generated/android/alloy/controllers/index.js
+++ test/apps/testing/ALOY-1237/_generated/android/alloy/controllers/index.js
@@ -1,115 +1,73 @@
-var Alloy = require('/alloy'),
-Backbone = Alloy.Backbone,
-_ = Alloy._;
-
-
-
-
-function __processArg(obj, key) {
-  var arg = null;
-  if (obj) {
-    arg = obj[key] || null;
-  }
-  return arg;
-}
-
-function Controller() {
-
-  require('/controllers/' + 'BaseController').apply(this, Array.prototype.slice.call(arguments));
-  this.__controllerPath = 'index';
-  this.args = arguments[0] || {};
-
-  if (arguments[0]) {
-    var __parentSymbol = __processArg(arguments[0], '__parentSymbol');
-    var $model = __processArg(arguments[0], '$model');
-    var __itemTemplate = __processArg(arguments[0], '__itemTemplate');
-  }
-  var $ = this;
-  var exports = {};
-  var __defers = {};
-
-  // Generated code that must be executed before all UI and/or
-  // controller code. One example is all model and collection
-  // declarations from markup.
-
-
-  // <dataFunctions>
-
-  // </dataFunctionsCode>
-
-  // Generated UI code
-  $.__views["index"] = Ti.UI.createWindow(
-  { backgroundColor: "#fff", id: "index" });
-
-  $.__views["index"] && $.addTopLevelView($.__views["index"]);
-  doOpen ? $.addListener($.__views["index"], 'open', doOpen) : __defers['$.__views["index"]!open!doOpen'] = true;$.__views["__alloyId0"] = Ti.UI.createLabel(
-  { text: 'Remove the all events in this controller' });
-
-  $.__views["index"].add($.__views["__alloyId0"]);
-  doClick ? $.addListener($.__views["__alloyId0"], 'click', doClick) : __defers['$.__views["__alloyId0"]!click!doClick'] = true;exports.destroy = function () {};
-
-
-  // make all IDed elements in $.__views available right on the $ in a
-  // controller's internal code. Externally the IDed elements will
-  // be accessed with getView().
-  _.extend($, $.__views);
-
-  // Controller code directly from the developer's controller file
-  function doOpen() {
-    var button = Ti.UI.createButton({
-      right: 10,
-      bottom: 10,
-      left: 10,
-      title: 'button' });
-
-    $.index.add(button);
-    $.addListener(button, 'click', function (e) {
-      // __trackId testing dummy event
-      e.cancelBubble = true;
-    });
-
-    // manually add event
-    $.addListener($.index, 'click', doWindowClick);
-
-    $.addListener($.index, 'click', function () {
-      _.each($.getListener($.index), function (listener) {
-        if (listener.type === 'click' &&
-        listener.handler !== doWindowClick) {
-          console.log('This is anonymous callback function');
-          console.log(listener);
-        }
-      });
-    });
-
-    console.log('Showing the all events in this controller');
-    console.log($.getListener());
-  }
-
-  // Once event fire, you do not fire again.
-  // All events to this window have been removed.
-  function doWindowClick() {
-    $.removeListener($.index);
-    console.log('Removed the all events on this window');
-  }
-
-  // Once event fire, you do not fire again.
-  // All events was nothing in this controller.
-  function doClick() {
-    $.removeListener();
-    console.log('Removed the all events in this controller');
-  }
-
-  $.index.open();
-
-  // Generated code that must be executed after all UI and
-  // controller code. One example deferred event handlers whose
-  // functions are not defined until after the controller code
-  // is executed.
-  __defers['$.__views["index"]!open!doOpen'] && $.addListener($.__views["index"], 'open', doOpen);__defers['$.__views["__alloyId0"]!click!doClick'] && $.addListener($.__views["__alloyId0"], 'click', doClick);
-
-  // Extend the $ instance with all functions and properties
-  // defined on the exports object.
-  _.extend($, exports);
-}
-
-module.exports = Controller;
+"use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var indexAbstract_1 = require("/controllers/indexAbstract");
+var _ = __importStar(require("underscore"));
+var index = /** @class */ (function (_super) {
+    __extends(index, _super);
+    function index() {
+        var _this = _super.call(this) || this;
+        // Once event fire, you do not fire again.
+        // All events to this window have been removed.
+        _this.doWindowClick = function () {
+            _this.removeListener(_this.index);
+            console.log('Removed the all events on this window');
+        };
+        _this.index.open();
+        return _this;
+    }
+    index.prototype.doOpen = function () {
+        var _this = this;
+        var button = Ti.UI.createButton({
+            right: 10,
+            bottom: 10,
+            left: 10,
+            title: 'button'
+        });
+        this.index.add(button);
+        this.addListener(button, 'click', function (e) {
+            // __trackId testing dummy event
+            e.cancelBubble = true;
+        });
+        // manually add event
+        this.addListener(this.index, 'click', this.doWindowClick);
+        this.addListener(this.index, 'click', function () {
+            _.each(_this.getListener(_this.index), function (listener) {
+                if (listener.type === 'click' &&
+                    listener.handler !== _this.doWindowClick) {
+                    console.log('This is anonymous callback function');
+                    console.log(listener);
+                }
+            });
+        });
+        console.log('Showing the all events in this controller');
+        console.log(this.getListener());
+    };
+    // Once event fire, you do not fire again.
+    // All events was nothing in this controller.
+    index.prototype.doClick = function () {
+        this.removeListener();
+        console.log('Removed the all events in this controller');
+    };
+    return index;
+}(indexAbstract_1.indexAbstract));
+exports.default = index;
