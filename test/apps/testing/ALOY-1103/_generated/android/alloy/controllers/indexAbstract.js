7e933d2fc981c29401affdd2c2ce6d29553bac46
Sergey Volkov <s.volkov@netris.ru>
2020-02-26 12:41:54 +0300

    feat(typescript): update test ALOY-1103

---
+++ test/apps/testing/ALOY-1103/_generated/android/alloy/controllers/indexAbstract.js
@@ -0,0 +1,98 @@
+"use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var alloy_1 = __importDefault(require("alloy"));
+var BaseController_1 = __importDefault(require("/controllers/BaseController"));
+var indexAbstract = /** @class */ (function (_super) {
+    __extends(indexAbstract, _super);
+    function indexAbstract(args) {
+        var _this = _super.call(this) || this;
+        _this.args = args;
+        _this.__controllerPath = indexAbstract.controllerPath;
+        _this.__parentSymbol = args && args['__parentSymbol'] ? args['__parentSymbol'] : null;
+        _this.$model = args && args['$model'] ? args['$model'] : null;
+        _this.__itemTemplate = args && args['__itemTemplate'] ? args['__itemTemplate'] : null;
+        // Generated code that must be executed before all UI and/or
+        // controller code. One example is all model and collection
+        // declarations from markup.
+        // <precode>
+        // </precode>
+        // Generated UI code
+        // <viewCode>
+        _this["index"] = Ti.UI.createWindow({ backgroundColor: "white", layout: "vertical", id: "index", });
+        _this["index"] && _this.addTopLevelView(_this["index"]);
+        if (alloy_1.default.isTablet) {
+            _this["__alloyId2"] = Ti.UI.createView({});
+            _this["__alloyId3"] = Ti.UI.createLabel({ font: { fontSize: "20dp", }, text: 'HeaderView Tablet', });
+            _this["__alloyId2"].add(_this["__alloyId3"]);
+        }
+        if (!alloy_1.default.isTablet) {
+            _this["__alloyId5"] = Ti.UI.createView({});
+            _this["__alloyId6"] = Ti.UI.createLabel({ font: { fontSize: "20dp", }, text: 'HeaderView Handheld', });
+            _this["__alloyId5"].add(_this["__alloyId6"]);
+        }
+        _this["__alloyId0"] = Ti.UI.createTableView((function () {
+            var o = {};
+            if (alloy_1.default.isTablet)
+                alloy_1.default.deepExtend(true, o, { headerView: _this["__alloyId2"], });
+            if (alloy_1.default.isHandheld)
+                alloy_1.default.deepExtend(true, o, { headerView: _this["__alloyId5"], });
+            alloy_1.default.deepExtend(true, o, { height: "80%", });
+            return o;
+        })());
+        _this["index"].add(_this["__alloyId0"]);
+        if (alloy_1.default.isTablet) {
+            _this["__alloyId7"] = Ti.UI.createView({});
+            _this["index"].add(_this["__alloyId7"]);
+            _this["__alloyId8"] = Ti.UI.createLabel({ font: { fontSize: "20dp", }, text: 'View Tablet', });
+            _this["__alloyId7"].add(_this["__alloyId8"]);
+        }
+        if (!alloy_1.default.isTablet) {
+            _this["__alloyId9"] = Ti.UI.createView({});
+            _this["index"].add(_this["__alloyId9"]);
+            _this["__alloyId10"] = Ti.UI.createLabel({ font: { fontSize: "20dp", }, text: 'View Handheld', });
+            _this["__alloyId9"].add(_this["__alloyId10"]);
+        }
+        // </viewCode>
+        // Populating $.__views for backward compatibility
+        // <__views>
+        _this.__views["__alloyId3"] = _this.__alloyId3;
+        _this.__views["__alloyId2"] = _this.__alloyId2;
+        _this.__views["__alloyId6"] = _this.__alloyId6;
+        _this.__views["__alloyId5"] = _this.__alloyId5;
+        _this.__views["__alloyId0"] = _this.__alloyId0;
+        _this.__views["__alloyId8"] = _this.__alloyId8;
+        _this.__views["__alloyId7"] = _this.__alloyId7;
+        _this.__views["__alloyId10"] = _this.__alloyId10;
+        _this.__views["__alloyId9"] = _this.__alloyId9;
+        _this.__views["index"] = _this.index;
+        return _this;
+        // </__view>
+        // Generated code that must be executed after all UI and
+        // controller code. One example deferred event handlers whose
+        // functions are not defined until after the controller code
+        // is executed.
+        // <postCode>
+        // </postcode>
+    }
+    indexAbstract.prototype.destroy = function () {
+    };
+    indexAbstract.controllerPath = 'index';
+    return indexAbstract;
+}(BaseController_1.default));
+exports.indexAbstract = indexAbstract;
--- test/apps/testing/ALOY-1103/_generated/ios/alloy/controllers/index.js
+++ test/apps/testing/ALOY-1103/_generated/ios/alloy/controllers/index.js
@@ -1,115 +1,26 @@
-var Alloy = require('/alloy'),
-Backbone = Alloy.Backbone,
-_ = Alloy._;
-
-
-
-
-function __processArg(obj, key) {
-  var arg = null;
-  if (obj) {
-    arg = obj[key] || null;
-  }
-  return arg;
-}
-
-function Controller() {
-
-  require('/controllers/' + 'BaseController').apply(this, Array.prototype.slice.call(arguments));
-  this.__controllerPath = 'index';
-  this.args = arguments[0] || {};
-
-  if (arguments[0]) {
-    var __parentSymbol = __processArg(arguments[0], '__parentSymbol');
-    var $model = __processArg(arguments[0], '$model');
-    var __itemTemplate = __processArg(arguments[0], '__itemTemplate');
-  }
-  var $ = this;
-  var exports = {};
-  var __defers = {};
-
-  // Generated code that must be executed before all UI and/or
-  // controller code. One example is all model and collection
-  // declarations from markup.
-
-
-  // <dataFunctions>
-
-  // </dataFunctionsCode>
-
-  // Generated UI code
-  $.__views["index"] = Ti.UI.createWindow(
-  { backgroundColor: "white", layout: "vertical", id: "index" });
-
-  $.__views["index"] && $.addTopLevelView($.__views["index"]);
-  if (Alloy.isTablet) {
-    $.__views["__alloyId2"] = Ti.UI.createView(
-    {});
-
-    $.__views["__alloyId3"] = Ti.UI.createLabel(
-    { font: { fontSize: "20dp", color: "black" }, text: 'HeaderView Tablet' });
-
-    $.__views["__alloyId2"].add($.__views["__alloyId3"]);
-  }
-  if (!Alloy.isTablet) {
-    $.__views["__alloyId5"] = Ti.UI.createView(
-    {});
-
-    $.__views["__alloyId6"] = Ti.UI.createLabel(
-    { font: { fontSize: "20dp", color: "black" }, text: 'HeaderView Handheld' });
-
-    $.__views["__alloyId5"].add($.__views["__alloyId6"]);
-  }
-  $.__views["__alloyId0"] = Ti.UI.createTableView(
-  function () {
-    var o = {};
-    if (Alloy.isTablet) Alloy.deepExtend(true, o, { headerView: $.__views["__alloyId2"] });
-    if (Alloy.isHandheld) Alloy.deepExtend(true, o, { headerView: $.__views["__alloyId5"] });
-    Alloy.deepExtend(true, o, { height: "80%" });
-    return o;
-  }());
-
-  $.__views["index"].add($.__views["__alloyId0"]);
-  if (Alloy.isTablet) {
-    $.__views["__alloyId7"] = Ti.UI.createView(
-    {});
-
-    $.__views["index"].add($.__views["__alloyId7"]);
-    $.__views["__alloyId8"] = Ti.UI.createLabel(
-    { font: { fontSize: "20dp", color: "black" }, text: 'View Tablet' });
-
-    $.__views["__alloyId7"].add($.__views["__alloyId8"]);
-  }
-  if (!Alloy.isTablet) {
-    $.__views["__alloyId9"] = Ti.UI.createView(
-    {});
-
-    $.__views["index"].add($.__views["__alloyId9"]);
-    $.__views["__alloyId10"] = Ti.UI.createLabel(
-    { font: { fontSize: "20dp", color: "black" }, text: 'View Handheld' });
-
-    $.__views["__alloyId9"].add($.__views["__alloyId10"]);
-  }
-  exports.destroy = function () {};
-
-
-  // make all IDed elements in $.__views available right on the $ in a
-  // controller's internal code. Externally the IDed elements will
-  // be accessed with getView().
-  _.extend($, $.__views);
-
-  // Controller code directly from the developer's controller file
-  $.index.open();
-
-  // Generated code that must be executed after all UI and
-  // controller code. One example deferred event handlers whose
-  // functions are not defined until after the controller code
-  // is executed.
-
-
-  // Extend the $ instance with all functions and properties
-  // defined on the exports object.
-  _.extend($, exports);
-}
-
-module.exports = Controller;
+"use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+Object.defineProperty(exports, "__esModule", { value: true });
+var indexAbstract_1 = require("/controllers/indexAbstract");
+var index = /** @class */ (function (_super) {
+    __extends(index, _super);
+    function index() {
+        var _this = _super.call(this) || this;
+        _this.index.open();
+        return _this;
+    }
+    return index;
+}(indexAbstract_1.indexAbstract));
+exports.default = index;
