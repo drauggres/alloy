32d1e5f40c9a8d76cbe907b7ee6ff78241f54962
Sergey Volkov <s.volkov@netris.ru>
2020-02-27 14:38:20 +0300

    feat(typescript): WebView.onlink
    refs ALOY-1697

--- Alloy/commands/compile/parsers/Ti.UI.WebView.js
+++ Alloy/commands/compile/parsers/Ti.UI.WebView.js
@@ -1,4 +1,5 @@
 const styler = require('../styler');
+const CU = require('../compilerUtils');
 
 exports.parse = function(node, state) {
 	return require('./base').parse(node, state, parse);
@@ -7,7 +8,17 @@ exports.parse = function(node, state) {
 function parse(node, state, args) {
 	const linkHandler = node.getAttribute('onlink');
 	if (linkHandler) {
-		node.setAttribute('onlink', `${styler.STYLE_EXPR_PREFIX}${linkHandler}`);
+		const value = state.outputFormat === 'TS' ? 'this.' + linkHandler + '.bind(this)' : linkHandler;
+		node.setAttribute('onlink', `${styler.STYLE_EXPR_PREFIX}${value}`);
+		var method = `abstract ${linkHandler}(e: OnLinkURLResponse): void;\n`;
+		var index = CU.abstractMethods.indexOf(`abstract ${linkHandler}(`);
+		if (index === -1) {
+			CU.abstractMethods += method;
+		} else if (CU.abstractMethods.indexOf(method) === -1) {
+			var before = CU.abstractMethods.substr(0, index);
+			var after = CU.abstractMethods.substr(index, CU.abstractMethods.length);
+			CU.abstractMethods = before + method + after;
+		}
 	}
 	return require('./default').parse(node, state, args);
 }
