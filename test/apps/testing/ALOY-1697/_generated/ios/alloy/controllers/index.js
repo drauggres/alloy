688178979520c7dff9c6c1b2540a625edb94be96
Sergey Volkov <s.volkov@netris.ru>
2020-02-27 14:37:46 +0300

    feat(typescript): update test ALOY-1697

--- test/apps/testing/ALOY-1697/_generated/ios/alloy/controllers/index.js
+++ test/apps/testing/ALOY-1697/_generated/ios/alloy/controllers/index.js
@@ -1,93 +1,46 @@
-var Alloy = require('/alloy'),
-Backbone = Alloy.Backbone,
-_ = Alloy._;
-
-
-
-
-function __processArg(obj, key) {
-  var arg = null;
-  if (obj) {
-    arg = obj[key] || null;
-  }
-  return arg;
-}
-
-function Controller() {
-
-  require('/controllers/' + 'BaseController').apply(this, Array.prototype.slice.call(arguments));
-  this.__controllerPath = 'index';
-  this.args = arguments[0] || {};
-
-  if (arguments[0]) {
-    var __parentSymbol = __processArg(arguments[0], '__parentSymbol');
-    var $model = __processArg(arguments[0], '$model');
-    var __itemTemplate = __processArg(arguments[0], '__itemTemplate');
-  }
-  var $ = this;
-  var exports = {};
-  var __defers = {};
-
-  // Generated code that must be executed before all UI and/or
-  // controller code. One example is all model and collection
-  // declarations from markup.
-
-
-  // <dataFunctions>
-
-  // </dataFunctionsCode>
-
-  // Generated UI code
-  $.__views["index"] = Ti.UI.createWindow(
-  { backgroundColor: "#fff", fullscreen: false, exitOnClose: true, id: "index" });
-
-  $.__views["index"] && $.addTopLevelView($.__views["index"]);
-  $.__views["webview"] = Ti.UI.createWebView(
-  { id: "webview", onlink: onLinkHandler });
-
-  $.__views["index"].add($.__views["webview"]);
-  exports.destroy = function () {};
-
-
-  // make all IDed elements in $.__views available right on the $ in a
-  // controller's internal code. Externally the IDed elements will
-  // be accessed with getView().
-  _.extend($, $.__views);
-
-  // Controller code directly from the developer's controller file
-  var htmlText =
-  '<!DOCTYPE html>\n' +
-  '<html>\n' +
-  '	<head>\n' +
-  '		<meta name="viewport" content="width=device-width, initial-scale=1.0">\n' +
-  '	</head>\n' +
-  '	<body>\n' +
-  '		<p>WebView "onlink" Test</p>\n' +
-  '		<br/>\n' +
-  '		<br/>\n' +
-  '		<a href="mylink://show/alert">Show Alert</a>\n' +
-  '	</body>\n' +
-  '</html>\n';
-
-  function onLinkHandler(e) {
-    if (e.url === 'mylink://show/alert') {
-      alert("The 'onLinkHandler' callback was invoked.");
-      return false;
+"use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+Object.defineProperty(exports, "__esModule", { value: true });
+var indexAbstract_1 = require("/controllers/indexAbstract");
+var htmlText = '<!DOCTYPE html>\n' +
+    '<html>\n' +
+    '	<head>\n' +
+    '		<meta name="viewport" content="width=device-width, initial-scale=1.0">\n' +
+    '	</head>\n' +
+    '	<body>\n' +
+    '		<p>WebView "onlink" Test</p>\n' +
+    '		<br/>\n' +
+    '		<br/>\n' +
+    '		<a href="mylink://show/alert">Show Alert</a>\n' +
+    '	</body>\n' +
+    '</html>\n';
+var index = /** @class */ (function (_super) {
+    __extends(index, _super);
+    function index() {
+        var _this = _super.call(this) || this;
+        _this.webview.html = htmlText;
+        _this.index.open();
+        return _this;
     }
-    return true;
-  }
-  $.webview.html = htmlText;
-  $.index.open();
-
-  // Generated code that must be executed after all UI and
-  // controller code. One example deferred event handlers whose
-  // functions are not defined until after the controller code
-  // is executed.
-
-
-  // Extend the $ instance with all functions and properties
-  // defined on the exports object.
-  _.extend($, exports);
-}
-
-module.exports = Controller;
+    index.prototype.onLinkHandler = function (e) {
+        if (e.url === 'mylink://show/alert') {
+            alert("The 'onLinkHandler' callback was invoked.");
+            return false;
+        }
+        return true;
+    };
+    return index;
+}(indexAbstract_1.indexAbstract));
+exports.default = index;
